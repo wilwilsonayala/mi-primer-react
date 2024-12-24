c21a5774a34dfbfa792a0d76f4f27a05aa6c8d07
wilwilson <wilson.ayala@uab.edu.bo>
2024-12-24 14:59:34 -0400

    Initial commit

---
+++ src/componentes/Header/header.js
@@ -0,0 +1,9 @@
+import "./header.css"
+
+function Header() {
+    return <header className="header">
+        <img src="/img/propaganda.png" alt='Org' />
+    </header>
+}
+
+export default Header
---
+++ src/componentes/footer/index.jsx
@@ -0,0 +1,22 @@
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="redes">
+                <a href="https://www.facebook.com/wilson.ayala.127/">
+                    <img src="/img/facebook.png" alt="Facebook" />
+                </a>
+                <a href="https://github.com/wilwilsonayala">
+                    <img src="/img/github.png" alt="GitHub" />
+                </a>
+                <a href="https://wa.me/73241949">
+                    <img src="/img/whatssap.png" alt="Whatssap" />
+                </a>
+            </div>
+            <img className="logofooter" src="/img/sinfondo.png" alt="org" />
+            <strong>Desarrollado por W@P</strong>
+        </footer>
+    );
+};
+export default Footer
